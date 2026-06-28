9949f40fb491ba4451120bfb071c9044cde6c4e1
AchiJap <achi.japaridze1988@gmail.com>
2026-06-28 21:20:32 +0400

    site root gets redirected to /home

--- src/main/java/com/quizwebsite/web/HomeController.java
+++ src/main/java/com/quizwebsite/web/HomeController.java
@@ -3,9 +3,15 @@ package com.quizwebsite.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/** The logged-in landing page. */
+/** The logged-in home page */
 @Controller
 public class HomeController {
+
+    /* redirect to home page. User not logged in is handled by the auth interceptor */
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/home";
+    }
 
     @GetMapping("/home")
     public String home() {
